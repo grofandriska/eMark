9541fe9861851082eabd4da85f514b1fa52d179b
Gróf András <andrasgrof93@gmail.com>
2021-08-20 10:13:03 +0200

    remove Modell Mapper
    never used

--- src/main/java/com/codecool/vizsgaremek/EKretaApplication.java
+++ src/main/java/com/codecool/vizsgaremek/EKretaApplication.java
@@ -1,19 +1,14 @@
 package com.codecool.vizsgaremek;
 
-import org.modelmapper.ModelMapper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class EKretaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EKretaApplication.class, args);
 	}
 
-	@Bean
-	public ModelMapper getModelMapper(){
-		return new ModelMapper();
-	}
 }
